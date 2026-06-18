00fe7c2a8e86af048a73e3eba6148ae2277b2b29
harinagendra02 <harinagendra486@gmail.com>
2026-06-18 10:31:42 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
-print("Bot Started")
+import os
+import requests
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHANNEL_ID = os.environ["CHANNEL_ID"]
+
+message = "✅ GitHub Bot Connected Successfully!\n\nBench Sales Job Bot is Ready 🚀"
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHANNEL_ID,
+        "text": message
+    }
+)
+
+print(response.text)
